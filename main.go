b2f8ec3193042bb860375c23f6a9a86837952b3f
Aaron Traas <aaron@traas.org>
2019-02-15 21:08:48 -0500

    Stubs

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+    "encoding/json"
+    "log"
+    "net/http"
+    "github.com/gorilla/mux"
+)
+
+type Endpoint struct {
+    Path        string      `json:"path"`
+    Method      string      `json:"method"`
+    Parameters  []Parameter `json:"params,omitempty"`
+    Description string      `json:"description,omitempty"`
+}
+
+type Parameter struct {
+    Name        string  `json:"name"`
+    Description string  `json:"description,omitempty"`
+}
+
+const SIDE = 3
+
+type GameTileState string
+
+const (
+    State_Empty GameTileState = ""
+    State_X     GameTileState = "X"
+    State_O     GameTileState = "O"
+)
+
+type GameBoardState [SIDE][SIDE]GameTileState
+
+var endpoints []Endpoint
+
+func Default(w http.ResponseWriter, r *http.Request) {
+    json.NewEncoder(w).Encode(endpoints)
+}
+
+func GetGame(w http.ResponseWriter, r *http.Request) {
+    var board GameBoardState
+    json.NewEncoder(w).Encode(board)
+}
+
+// our main function
+func main() {
+    endpoints = append(endpoints, Endpoint{
+        Path: "/",
+        Method: "GET",
+        Description: "This endpoint. Lists all of the possible endpoints."})
+
+    endpoints = append(endpoints, Endpoint{
+        Path: "/game",
+        Method: "GET",
+        Parameters: []Parameter{
+            Parameter{Name: "game-id"}},
+        Description: "This endpoint. Lists all of the possible endpoints."})
+
+    router := mux.NewRouter()
+    router.HandleFunc("/", Default).Methods("GET")
+    router.HandleFunc("/game", GetGame).Methods("GET")
+    log.Fatal(http.ListenAndServe(":8000", router))
+}
